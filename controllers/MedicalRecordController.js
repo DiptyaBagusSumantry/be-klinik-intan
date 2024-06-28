049abcab27bea70b12261f23af0538fc3277a773
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-06-29 00:09:45 +0700

    handle update transaction

--- controllers/MedicalRecordController.js
+++ controllers/MedicalRecordController.js
@@ -11,6 +11,7 @@ const { paginator } = require("../helper/Pagination.js");
 const { searchWhere } = require("../helper/Search.js");
 const moment = require("moment");
 
+
 class MedicalRecord {
   static async createMedicalRecord(req, res) {
     try {
@@ -28,9 +29,9 @@ class MedicalRecord {
       } = req.body;
 
       const chekReservation = await Models.Reservation.findOne({
-        where: {id: idReservasi, status: false}
-      })
-      if(!chekReservation){
+        where: { id: idReservasi, status: false },
+      });
+      if (!chekReservation) {
         return handlerError(res, {
           message:
             "Medical record already created in today or patient not yet create reservation in today, please check get medical record! or chek get reservation!",
@@ -141,27 +142,43 @@ class MedicalRecord {
         statusPembayaran,
         kodeDiagnosa,
       } = req.body;
+
+      const getDetailRM = await MedicalRecords.findOne({
+        where: {
+          id: req.params.id,
+        },
+        raw: true,
+      });
+      const getTransaction = await Models.Transaction.findOne({
+        where: {
+          medicalRecordId: req.params.id,
+        },
+        raw: true,
+      });
+      console.log(getDetailRM)
+      console.log(getTransaction)
       const biaya = biayaLayanan + biayaObat;
       await MedicalRecords.update(
         {
-          pelayanan,
-          keluhan,
-          diagnosa,
-          tindakan,
-          patientId,
-          kode_diagnosa: kodeDiagnosa,
+          pelayanan: pelayanan ? pelayanan : getDetailRM.pelayanan,
+          keluhan: keluhan ? keluhan : getDetailRM.keluhan,
+          diagnosa: diagnosa ? diagnosa : getDetailRM.diagnosa,
+          tindakan: tindakan ? tindakan : getDetailRM.tindakan,
+          patientId: patientId ? patientId : getDetailRM.patientId,
+          kode_diagnosa: kodeDiagnosa ? kodeDiagnosa : getDetailRM.kode_diagnosa,
         },
         {
           where: { id: req.params.id },
         }
       );
+    
       const updateTransactioon = await Models.Transaction.update(
         {
-          total_payment: biaya,
+          total_payment: biaya ? biaya : getTransaction.total_payment,
           invoice: `${new Date().getTime()}`,
           status: statusPembayaran ? statusPembayaran : "BELUM LUNAS",
-          purchased: JSON.stringify({ biayaLayanan, biayaObat }),
-          patientId,
+          purchased: JSON.stringify({ biayaLayanan, biayaObat }) ? JSON.stringify({ biayaLayanan, biayaObat }) : getTransaction.purchased,
+          patientId: patientId ? patientId : getDetailRM.patientId,
         },
         {
           where: { medicalRecordId: req.params.id },
