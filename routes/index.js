08b819fd6ccb7fe9dd2663c42a1889d082108ed4
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-08-05 22:48:18 +0700

    oke

--- routes/index.js
+++ routes/index.js
@@ -24,33 +24,33 @@ router.get("/fetch", verifyToken, AuthController.Fetch);
 router.get(
   "/amount-dashboard",
   verifyToken,
-  IsAdmin,
+
   UserController.amountDashboard
 );
-router.get("/role", verifyToken, IsAdmin, UserController.getRole);
+router.get("/role", verifyToken, UserController.getRole);
 router.post(
   "/user-management",
   verifyToken,
-  IsAdmin,
+
   UserController.createUser
 );
-router.get("/user-management", verifyToken, IsAdmin, UserController.getUser);
+router.get("/user-management", verifyToken, UserController.getUser);
 router.get(
   "/user-management/:id",
   verifyToken,
-  IsAdmin,
+
   UserController.getUserById
 );
 router.put(
   "/user-management/:id",
   verifyToken,
-  IsAdmin,
+
   UserController.updateUser
 );
 router.delete(
   "/user-management/:id",
   verifyToken,
-  IsAdmin,
+
   UserController.deleteUser
 );
 
@@ -79,19 +79,16 @@ router.get("/dokter", verifyToken, JadwalDokterController.getDokter);
 router.post(
   "/jadwal-dokter",
   verifyToken,
-  IsAdmin,
+  
   JadwalDokterController.createJadwalDokter
 );
 router.put(
   "/jadwal-dokter/:id",
   verifyToken,
-  IsAdmin,
+  
   JadwalDokterController.updateJadwalDokter
 );
-router.get(
-  "/jadwal-dokter",
-  JadwalDokterController.getJadwalDokter
-);
+router.get("/jadwal-dokter", JadwalDokterController.getJadwalDokter);
 router.get(
   "/jadwal-dokter/:id",
   verifyToken,
@@ -100,7 +97,7 @@ router.get(
 router.delete(
   "/jadwal-dokter/:id",
   verifyToken,
-  IsAdmin,
+  
   JadwalDokterController.deleteJadwalDokter
 );
 
@@ -108,7 +105,7 @@ router.get("/patient", verifyToken, PatientController.getPatient);
 router.get(
   "/patient/:id",
   verifyToken,
-  IsAdmin,
+  
   PatientController.detailPatient
 );
 router.put(
@@ -122,7 +119,7 @@ router.get("/check-patient", verifyToken, PatientController.chekPatient);
 router.delete(
   "/patient/:id",
   verifyToken,
-  IsAdmin,
+  
   PatientController.deletePatient
 );
 
@@ -147,12 +144,25 @@ router.get(
   ReservationController.getDetailReservation
 );
 
-router.get("/type-reservation/:type", verifyToken, ReservationController.getReservation);
-router.post("/service", verifyToken, IsAdmin, ServiceController.createService);
-router.put("/service/:id", verifyToken, IsAdmin, ServiceController.updateService);
+router.get(
+  "/type-reservation/:type",
+  verifyToken,
+  ReservationController.getReservation
+);
+router.post("/service", verifyToken,  ServiceController.createService);
+router.put(
+  "/service/:id",
+  verifyToken,
+  
+  ServiceController.updateService
+);
 router.get("/service", verifyToken, ServiceController.getService);
 router.get("/transaction", verifyToken, TransactionController.getTransaction);
-router.get("/transaction/:type", verifyToken, TransactionController.getTypeTransaction);
+router.get(
+  "/transaction/:type",
+  verifyToken,
+  TransactionController.getTypeTransaction
+);
 
 router.get(
   "/invoice-obat/:id",
